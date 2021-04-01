11831e3273577f1d52a0d63c5bfe9dd7a029164d
Kserhaja <kenanserhatlic@hotmail.com>
2021-04-01 09:57:07 +0200

    console. log option

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -1,26 +1,33 @@
-//getStorageItem/App.js
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./App.css";
 class App extends Component {
   setMyStorage = () => {
-    document.cookie = 'Year=2021';
-    localStorage.setItem('Paragon', 'yes, but Arena first');
-    sessionStorage.setItem('frontend', 'React');
-  }
+    document.cookie = ("Year", "2021");
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  };
   getMyStorage = () => {
-    // eslint-disable-next-line
-  const myCookieData = document.cookie.getItem('Year');
-  // eslint-disable-next-line
-  const myLocalStorageData = localStorage.getItem('Paragon');
-  // eslint-disable-next-line
-  const mySessionStorageData = sessionStorage.getItem('frontend');
-  }
+    let myCookieData = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("Year="))
+    .split("=")[1];
+    let myLocalStorageData = localStorage.getItem("Paragon");
+    let mySessionStorage = sessionStorage.getItem("frontend");
+    console.log(myCookieData, myLocalStorageData, mySessionStorage)
+  };
   render() {
     return (
-      <div className='App'>
-        <button type='button' onClick={this.setMyStorage}>Set my storage</button>
-        <button type='button' onClick={this.getMyStorage}>Get my storage</button>
+      <div className="App">
+        <button type="button" onClick={this.setMyStorage}>
+          Set cookie
+        </button>
+        <button type="button" onClick={this.getMyStorage}>
+
+        </button>
       </div>
+
     );
   }
 }
+
 export default App;
